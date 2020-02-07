d2b86024d3c675d4f8a805d2942f71cfcc2d52c0
Jason Coposky <jasonc@renci.org>
2020-02-07 11:19:06 -0500

    complete renaming, add testing

---
+++ example_tiering_invocation.r
@@ -0,0 +1,18 @@
+{
+	"policy" : "irods_policy_enqueue_rule",
+        "delay_conditions" : "<EF>REPEAT FOR EVER</EF>",
+	"payload" : {
+	    "policy" : "irods_policy_execute_rule",
+            "payload" : {
+	        "policy_to_invoke" : "irods_policy_storage_tiering",
+                "parameters" : {
+                    "tier_groups" : ["example_group" ]
+                 },
+                 "configuration" : {
+                 }
+             }
+        }
+}
+INPUT null
+OUTPUT ruleExecOut
+
