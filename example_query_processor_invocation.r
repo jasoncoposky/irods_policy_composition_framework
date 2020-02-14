31142fa0026f315160f88ce2f8530e3b8bd6cd44
Jason Coposky <jasonc@renci.org>
2020-02-14 15:50:51 -0500

    clean up

--- example_query_processor_invocation.r
+++ example_query_processor_invocation.r
@@ -5,8 +5,6 @@
 	    "policy" : "irods_policy_execute_rule",
             "payload" : {
 	        "policy_to_invoke" : "irods_policy_query_processor",
-                "policy_enforcement_point" : "pep_api_nopes",
-                "event_name" : "CREATE",
                 "parameters" : {
                     "query_string" : "SELECT COLL_NAME, DATA_NAME WHERE COLL_NAME like '/tempZone/home/rods%'",
                     "query_limit" : 10,
